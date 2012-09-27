e9a57704e4b36ff6806e3dd80bb22bcd7e5e4f8d
James N. V. Cash <james.nvc@gmail.com>
2012-09-27 14:23:56 -0400

    Getting build to work for iOS 6

---
+++ xcode/libmoai/libmoai-sphinx/libmoai_sphinx.h
@@ -0,0 +1,13 @@
+//
+//  libmoai_sphinx.h
+//  libmoai-sphinx
+//
+//  Created by James Cash on 26-09-12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libmoai_sphinx : NSObject
+
+@end
---
+++ xcode/libmoai/libmoai-sphinx/libmoai_sphinx.m
@@ -0,0 +1,13 @@
+//
+//  libmoai_sphinx.m
+//  libmoai-sphinx
+//
+//  Created by James Cash on 26-09-12.
+//
+//
+
+#import "libmoai_sphinx.h"
+
+@implementation libmoai_sphinx
+
+@end
